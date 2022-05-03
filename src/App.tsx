12a95346500f0f46e76a759cefe54d38e4295edf
LuisEnMarroquin <mluis651@gmail.com>
2022-05-03 07:52:55 -0500

    Creada mini aplicacion de notas

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
 
 function App() {
+  const [chats, setChats] = useState<string[]>([]);
+
+  const [name, setName] = useState("");
+  const handleChange = (event: any) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log("xd", event.target.value);
+    setChats((current) => [...current, name]);
+    setName("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Box component="h1" sx={{ textAlign: "center" }}>
+        Mini aplicación de notas
+      </Box>
+      <Box sx={{ py: "10px" }}>
+        <ul>
+          {chats.map((value, index) => (
+            <Box component="li" key={index}>
+              {value}
+            </Box>
+          ))}
+        </ul>
+      </Box>
+      <Box sx={{ py: "20px" }}>
+        <TextField
+          fullWidth
+          label="Mensaje"
+          variant="outlined"
+          value={name}
+          onChange={handleChange}
+        />
+      </Box>
+      <Box sx={{ py: "20px", textAlign: "center" }}>
+        <Button variant="contained" onClick={handleSubmit}>
+          Guardar
+        </Button>
+      </Box>
+    </Container>
   );
 }
 
